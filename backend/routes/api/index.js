0d716fa68b13985e7c28b903204767752dac0fa2
Wooyoung Kim <wooyoungkim24@gmail.com>
2022-03-02 14:35:52 -0500

    first commit

---
+++ backend/routes/api/index.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+
+// router.get(
+//   '/restore-user',
+//   restoreUser,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+
+
+// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       }
+//     });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+
+
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
+
+module.exports = router;
---
+++ frontend/src/store/index.js
@@ -0,0 +1,33 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+import sessionReducer from './session';
+// import businessReducer from "./business";
+// import reviewsReducer from "./review"
+
+const rootReducer = combineReducers({
+  session: sessionReducer,
+//   businesses: businessReducer,
+//   reviews: reviewsReducer
+});
+
+let enhancer;
+
+if (process.env.NODE_ENV === "production") {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = require("redux-logger").default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
+
+
+
+
+
+export default configureStore;
